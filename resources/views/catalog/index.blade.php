bdb7008b5cf2e616c9e45a5d07fd7739a3b77847
Ng Jia Sheng <ngjiasheng38@email.com>
2022-10-12 14:36:58 +0800

    fix: catalog image not showing

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -27,7 +27,7 @@
             @foreach($books as $book)
             <div class="col-md-4">
                   <div class="card mb-4 box-shadow">
-                        <img class="card-img-top" alt="" src="{{ $book->image_path }}" style="height: 225px; width: 100%; display: block;">
+                        <img class="card-img-top" alt="" src="{{ asset('storage/' . $book->image_path) }}" style="height: 225px; width: 100%; display: block;">
                         <div class="card-body">
                               <p class="card-text">{{ $book->name }}</p>
                               <div class="d-flex justify-content-between align-items-center">
