19a98e652b146fede62f372200d05429093f0824
weiliang79 <liang2010@hotmail.my>
2022-11-22 14:00:02 +0800

    fix: admin can access order history page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,9 +86,11 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                @can('isUser')
                                 <a class="dropdown-item" href="{{ route('order_history') }}">
                                     {{ __('Order History') }}
                                 </a>
+                                @endcan
 
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
